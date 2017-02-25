a18930628844fb4f34ac3eeed519963586a4c207
Mantas Gudynas <mantasgudynas@tracsis.com>
2017-02-25 12:50:27 +0000

    Extracting Api

---
+++ Api.hs
@@ -0,0 +1,125 @@
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE PackageImports        #-}
+{-# LANGUAGE RecordWildCards       #-}
+{-# LANGUAGE TypeOperators         #-}
+{-# LANGUAGE GADTs                 #-}
+{-# LANGUAGE EmptyDataDecls        #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE FlexibleContexts      #-}
+
+module Api where
+
+import Data.Text        (Text)
+import Servant          hiding (Handler)
+import Yesod
+import GHC.Generics
+import Data.Swagger hiding (get)
+import Database.Persist.Sqlite
+import Control.Monad.Trans.Reader
+import Control.Monad.Trans.Except
+import GHC.Int (Int64)
+import Database
+
+--- Transformer Stack to DB access
+type AppM = ReaderT Config (ExceptT ServantErr IO)
+
+readerToEither :: Config -> AppM :~> ExceptT ServantErr IO
+readerToEither cfg = Nat $ \x -> runReaderT x cfg
+
+readerServer :: Config -> Server PersonAPI
+readerServer cfg = enter (readerToEither cfg) server
+
+-- Model and stuff
+data Entity' = Entity' { id :: Int, name :: String } deriving (Generic)
+instance ToJSON Entity'
+instance ToSchema Entity'
+
+data SampleRequest = SampleRequest { field1  :: String, field2 :: Maybe String } deriving (Generic)
+instance ToJSON SampleRequest
+instance FromJSON SampleRequest
+instance ToSchema SampleRequest
+
+---
+
+type PersonAPI =  GetEntities
+             :<|> GetEntity
+             :<|> Echo
+             :<|> ProcessRequest
+             :<|> WithHeader
+             :<|> WithError
+             :<|> ReturnHeader
+             :<|> AddCar
+             :<|> GetCars
+             :<|> GetCar
+
+
+server :: ServerT PersonAPI AppM
+server = getEntities
+    :<|> getEntity
+    :<|> echo
+    :<|> processRequest
+    :<|> withHeader
+    :<|> failingHandler
+    :<|> responseHeader
+    :<|> addCar
+    :<|> getCars
+    :<|> getCar
+
+
+type AddCar = "car" :> "add" :> Get '[PlainText] String
+addCar :: AppM String
+addCar = do
+  _ <- runDb $ insert $ Car "Foo"
+  return "foo"
+
+type GetCars = "car"  :> "list" :> Get '[JSON] [Car]
+getCars :: AppM [Car]
+getCars = runDb $ do
+    b <- selectList [] [Asc CarMake]
+    return $ map entityVal b
+
+type GetCar = "car" :> "get" :> Capture "id" Int64 :> Get '[JSON] (Maybe Car)
+getCar :: Int64 -> AppM (Maybe Car)
+getCar i = runDb $ get (toSqlKey i)
+
+
+-- Request handlers
+type GetEntities = "entity" :> "list"  :> Get '[JSON] [Entity']
+getEntities :: AppM [Entity']
+getEntities = return [ Entity' 1 "One" ]
+---
+type GetEntity =  "entity" :>  "get"  :> Capture "id" Int
+                                      :> Capture "name" String
+                                      :> Get '[JSON] Entity'
+getEntity :: Int -> String -> AppM Entity'
+getEntity i username = return $ Entity' i username
+---
+type Echo = "echo"        :> QueryParam "text" Text
+                          :> Get '[PlainText] String
+echo :: Maybe Text -> AppM String
+echo = return . show
+---
+type ProcessRequest = "process-request"   :> ReqBody '[JSON] SampleRequest
+                                          :> Post '[PlainText] String
+processRequest :: SampleRequest -> AppM String
+processRequest = return . field1
+---
+type WithHeader = "with-header"       :> Servant.Header "Header" String
+                                      :> Get '[PlainText] String
+withHeader :: Maybe String -> AppM String
+withHeader = return . show
+---
+type WithError = "with-error"        :> Get '[PlainText] String
+failingHandler :: AppM String
+failingHandler = throwError $ err401 { errBody = "Sorry dear user." }
+---
+type ReturnHeader = "return-header"     :> Get '[PlainText] (Headers '[Servant.Header "SomeHeader" String] String)
+responseHeader :: AppM (Headers '[Servant.Header "SomeHeader" String] String)
+responseHeader = return $ Servant.addHeader "headerVal" "foo"
--- Main.hs
+++ Main.hs
@@ -4,144 +4,30 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveGeneric #-}
-
 {-# LANGUAGE DataKinds             #-}
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE PackageImports        #-}
 {-# LANGUAGE RecordWildCards       #-}
 {-# LANGUAGE TypeOperators         #-}
-
 {-# LANGUAGE GADTs                 #-}
 {-# LANGUAGE EmptyDataDecls        #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE FlexibleContexts      #-}
 
+-- TODO: Tidy up language flags
+
 --import Yesod
-import           Data.Text        (Text)
 import           Servant          hiding (Handler)
 import           Yesod
 import           Yesod.Static
-import           GHC.Generics
 import           Data.Swagger hiding (get)
 import           Servant.Swagger
 import           Control.Lens
 
-import           Database.Persist.Sqlite
-import           Control.Monad.Trans.Reader
-import           Control.Monad.Trans.Except
-import GHC.Int (Int64)
-
 import EmbeddedAPI
 import Database
+import Api
 
-
-
-
-
-
-
-type AppM = ReaderT Config (ExceptT ServantErr IO)
-
-readerToEither :: Config -> AppM :~> ExceptT ServantErr IO
-readerToEither cfg = Nat $ \x -> runReaderT x cfg
-
-readerServer :: Config -> Server PersonAPI
-readerServer cfg = enter (readerToEither cfg) server
-
-
-
-
-type PersonAPI =  GetEntities
-             :<|> GetEntity
-             :<|> Echo
-             :<|> ProcessRequest
-             :<|> WithHeader
-             :<|> WithError
-             :<|> ReturnHeader
-             :<|> AddCar
-             :<|> GetCars
-             :<|> GetCar
-
-
-server :: ServerT PersonAPI AppM
-server = getEntities
-    :<|> getEntity
-    :<|> echo
-    :<|> processRequest
-    :<|> withHeader
-    :<|> failingHandler
-    :<|> responseHeader
-    :<|> addCar
-    :<|> getCars
-    :<|> getCar
-
-
-type AddCar = "car" :> "add" :> Get '[PlainText] String
-addCar :: AppM String
-addCar = do
-  _ <- runDb $ insert $ Car "Foo"
-  return "foo"
-
-type GetCars = "car"  :> "list" :> Get '[JSON] [Car]
-getCars :: AppM [Car]
-getCars = runDb $ do
-    b <- selectList [] [Asc CarMake]
-    return $ map entityVal b
-
-type GetCar = "car" :> "get" :> Capture "id" Int64 :> Get '[JSON] (Maybe Car)
-getCar :: Int64 -> AppM (Maybe Car)
-getCar i = runDb $ get (toSqlKey i)
-
--- Model and stuff
-data Entity' = Entity' { id :: Int, name :: String } deriving (Generic)
-instance ToJSON Entity'
-instance ToSchema Entity'
-
-data SampleRequest = SampleRequest { field1  :: String, field2 :: Maybe String } deriving (Generic)
-instance ToJSON SampleRequest
-instance FromJSON SampleRequest
-instance ToSchema SampleRequest
-
-
--- Request handlers
-type GetEntities = "entity" :> "list"  :> Get '[JSON] [Entity']
-getEntities :: AppM [Entity']
-getEntities = return [ Entity' 1 "One" ]
----
-type GetEntity =  "entity" :>  "get"  :> Capture "id" Int
-                                      :> Capture "name" String
-                                      :> Get '[JSON] Entity'
-getEntity :: Int -> String -> AppM Entity'
-getEntity i username = return $ Entity' i username
----
-type Echo = "echo"        :> QueryParam "text" Text
-                          :> Get '[PlainText] String
-echo :: Maybe Text -> AppM String
-echo = return . show
----
-type ProcessRequest = "process-request"   :> ReqBody '[JSON] SampleRequest
-                                          :> Post '[PlainText] String
-processRequest :: SampleRequest -> AppM String
-processRequest = return . field1
----
-type WithHeader = "with-header"       :> Servant.Header "Header" String
-                                      :> Get '[PlainText] String
-withHeader :: Maybe String -> AppM String
-withHeader = return . show
----
-type WithError = "with-error"        :> Get '[PlainText] String
-failingHandler :: AppM String
-failingHandler = throwError $ err401 { errBody = "Sorry dear user." }
----
-type ReturnHeader = "return-header"     :> Get '[PlainText] (Headers '[Servant.Header "SomeHeader" String] String)
-responseHeader :: AppM (Headers '[Servant.Header "SomeHeader" String] String)
-responseHeader = return $ Servant.addHeader "headerVal" "foo"
-
-
-
-
-
-------------------------------------------------
 
 data App = App { appAPI :: EmbeddedAPI
                , getStatic :: Static
@@ -164,7 +50,6 @@ getSwaggerR = return $ toJSON $ toSwagger (Proxy :: Proxy PersonAPI)
   & info.title   .~ "Todo API"
   & info.version .~ "1.0"
   & applyTags [Tag "API Controller" (Just "API Controller Name") Nothing]
-
 
 main :: IO ()
 main = do
