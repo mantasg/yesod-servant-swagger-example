eb04064365ab5e39a9e7a9af0c60fac0c64f258f
Mantas Gudynas <m.gudynas@tracsis.com>
2017-03-01 12:43:10 +0000

    externalised config

--- src/Main.hs
+++ src/Main.hs
@@ -2,18 +2,22 @@
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE DeriveGeneric #-}
 
 
 import           Servant          hiding (Handler)
 import           Yesod
 import           Yesod.Static
 import Data.Aeson
-import Data.ByteString.Lazy.Char8
+import Data.ByteString.Lazy.Char8 (unpack, pack)
+import GHC.Generics
 
 
 import EmbeddedAPI
 import Database
 import Api
+
 
 
 data App = App { appAPI :: EmbeddedAPI
@@ -35,11 +39,28 @@ getSwaggerR :: Handler String
 getSwaggerR = return $ unpack $ encode getSwagger
 
 
+data AppConfig = AppConfig { port :: Int, staticDirectory :: String } deriving (Generic, Show)
+instance FromJSON AppConfig
+
+
+defaultAppConfig :: AppConfig 
+defaultAppConfig = AppConfig 3000 "static"
+
+getConfig :: IO AppConfig
+getConfig = do 
+  fileContent <- readFile "config.json"
+  let appConfig = decode (pack fileContent) :: Maybe AppConfig
+  return $ case appConfig of 
+              (Just c) -> c
+              Nothing  -> defaultAppConfig
+  
+
 main :: IO ()
 main = do
+  appConfig <- getConfig  
   pool <- makeSqlitePool
   --pool <- makePostgresPool
   let myServer = readerServer (Config pool)
   let api = serve (Proxy :: Proxy CombinedAPI) myServer
-  static' <- static "static"
-  warp 3000 (App (EmbeddedAPI api) static')
+  static' <- static (staticDirectory appConfig)
+  warp (port appConfig) (App (EmbeddedAPI api) static')
